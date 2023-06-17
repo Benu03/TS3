689cfb747b175d7d37f9b2eea989f50a502b14b4
Your Name <you@example.com>
2023-06-18 01:15:49 +0700

    invoice bengkel

--- resources/views/admin-client/layout/menu.blade.php
+++ resources/views/admin-client/layout/menu.blade.php
@@ -10,7 +10,8 @@
   }
 </style>
 <!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background: linear-gradient(to bottom, #3f3f3f, #25872a);">
+<aside class="main-sidebar sidebar-dark-primary elevation-4" style="background: linear-gradient(to bottom, #3f3f3f, #25872a); border-top-right-radius: 15px;">
+  <!-- Brand Logo -->
     <!-- Brand Logo -->
     <a href="{{ asset('admin-client/dasbor') }}" class="brand-link">
       <img src="{{ asset('assets/upload/image/'.website('icon')) }}"
