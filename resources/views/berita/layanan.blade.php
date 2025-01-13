0abd670fb2dd2e3a605e07e90adbd107f087c00c
Your Name <ibnu.khoirin03@gmail.com>
2025-01-13 09:40:00 +0700

    map bengkel

--- resources/views/berita/layanan.blade.php
+++ resources/views/berita/layanan.blade.php
@@ -1,26 +1,3 @@
-<?php
-// Data Dummy untuk Lokasi di Grouping Berdasarkan Provinsi
-$locations = [
-    'DKI Jakarta' => [
-        ['id' => 1, 'name' => 'Jakarta Pusat', 'gmap' => '-6.186486,106.834091', 'address' => 'Jl. Merdeka No. 1, Jakarta Pusat'],
-        ['id' => 2, 'name' => 'Jakarta Selatan', 'gmap' => '-6.261493,106.810600', 'address' => 'Jl. Sudirman No. 2, Jakarta Selatan'],
-    ],
-    'Jawa Barat' => [
-        ['id' => 3, 'name' => 'Bandung', 'gmap' => '-6.917464,107.619123', 'address' => 'Jl. Asia Afrika No. 3, Bandung'],
-        ['id' => 4, 'name' => 'Bekasi', 'gmap' => '-6.238270,107.005042', 'address' => 'Jl. Ahmad Yani No. 4, Bekasi'],
-    ],
-    'Jawa Timur' => [
-        ['id' => 5, 'name' => 'Surabaya', 'gmap' => '-7.250445,112.768845', 'address' => 'Jl. Gubernur Suryo No. 5, Surabaya'],
-        ['id' => 6, 'name' => 'Malang', 'gmap' => '-7.966620,112.632632', 'address' => 'Jl. Ijen No. 6, Malang'],
-    ],
-];
-
-
-?>
-<?php 
-$bg   = DB::connection('ts3')->table('cp.heading')->where('halaman','Layanan')->orderBy('id_heading','DESC')->first();
- ?>
-
 
 <section class="wf100 p80 inner-header" style="background-image: url('{{ asset('assets/upload/image/'.$bg->gambar) }}'); background-position: bottom center;">
    <div class="container">
@@ -71,76 +48,6 @@ $bg   = DB::connection('ts3')->table('cp.heading')->where('halaman','Layanan')->
       
 
 
-            {{-- <div class="container">
-     
-            <div class="row mb-4">
-               <div class="col-lg-12">
-                  <div class="d-flex flex-wrap justify-content-start">
-                     <!-- Sumatera -->
-                     <div class="custom-margin-berita">
-                           <h6 class="font-weight-bold">Sumatera</h6>
-                           <ul class="list-unstyled mb-0">
-                              <li>Aceh</li>
-                              <li>Bengkulu</li>
-                              <li>Jambi</li>
-                              <li>Kepulauan Riau</li>
-                              <li>Kepulauan Bangka Belitung</li>
-                              <li>Lampung</li>
-                              <li>Riau</li>
-                              <li>Sumatera Barat</li>
-                              <li>Sumatera Selatan</li>
-                              <li>Sumatera Utara</li>
-                           </ul>
-                     </div>
-
-                     <div class="custom-margin-berita">
-                           <h6 class="font-weight-bold">Jawa</h6>
-                           <ul class="list-unstyled mb-0">
-                              <li>Banten</li>
-                              <li>Jakarta</li>
-                              <li>Jawa Barat</li>
-                              <li>Jawa Tengah</li>
-                              <li>Jawa Timur</li>
-                           </ul>
-                     </div>
-         
-                     <!-- Nusa Tenggara -->
-                     <div class="custom-margin-berita">
-                           <h6 class="font-weight-bold">Nusa Tenggara</h6>
-                           <ul class="list-unstyled mb-0">
-                              <li>Bali</li>
-                              <li>Nusa Tenggara Barat</li>
-                              <li>Nusa Tenggara Timur</li>
-                           </ul>
-                     </div>
-         
-                     <div class="custom-margin-berita">
-                           <h6 class="font-weight-bold">Kalimantan</h6>
-                           <ul class="list-unstyled mb-0">
-                              <li>Kalimantan Barat</li>
-                              <li>Kalimantan Selatan</li>
-                              <li>Kalimantan Tengah</li>
-                              <li>Kalimantan Timur</li>
-                              <li>Kalimantan Utara</li>
-                           </ul>
-                     </div>
-                     <div>
-                           <h6 class="font-weight-bold">Sulawesi</h6>
-                           <ul class="list-unstyled mb-0">
-                              <li>Gorontalo</li>
-                              <li>Sulawesi Barat</li>
-                              <li>Sulawesi Selatan</li>
-                              <li>Sulawesi Tengah</li>
-                              <li>Sulawesi Tenggara</li>
-                              <li>Sulawesi Utara</li>
-                           </ul>
-                     </div>
-                  </div>
-               </div>
-         </div>
-         </div> --}}
-     
-
        @endif
        @if($berita->sop_layanan != null)
          <div class="row text center">
@@ -177,107 +84,131 @@ $bg   = DB::connection('ts3')->table('cp.heading')->where('halaman','Layanan')->
       </section>
       <div class="clearfix"><br><br></div>
 
+      <script>
+         let map, markers = [];
+         const locations2 = @json($locations2);
+       
+         const customIcon = L.icon({
+             iconUrl: 'https://ts3.co.id/assets/upload/image/2.png',
+             iconSize: [32, 32],
+             iconAnchor: [16, 32],
+             popupAnchor: [0, -32]
+         });
+      
+         function initMap() {
+            map = L.map('map', {
+            center: [-2.600029, 118.015776], // Pusat Indonesia
+            zoom: 5, // Level zoom awal
+            minZoom: 4, // Set batas zoom keluar (lebih kecil)
+            maxZoom: 15 // Set batas zoom masuk (lebih besar)
+        });
+      
+             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                 attribution: '&copy; OpenStreetMap contributors'
+             }).addTo(map);
 
-<script>
-   let map, markers = [];
+             const markersCluster = L.markerClusterGroup({
+               iconCreateFunction: function(cluster) {
+                  const markersInCluster = cluster.getAllChildMarkers();
+                  const markerCount = markersInCluster.length;
 
-   const locations = [
-       { lat: -6.186486, lng: 106.834091, title: "Jakarta Pusat", address: "Jl. Merdeka No. 1, Jakarta Pusat" },
-       { lat: -6.261493, lng: 106.810600, title: "Jakarta Selatan", address: "Jl. Sudirman No. 2, Jakarta Selatan" },
-       { lat: -6.917464, lng: 107.619123, title: "Bandung", address: "Jl. Asia Afrika No. 3, Bandung" },
-       { lat: -6.238270, lng: 107.005042, title: "Bekasi", address: "Jl. Ahmad Yani No. 4, Bekasi" },
-       { lat: -7.250445, lng: 112.768845, title: "Surabaya", address: "Jl. Gubernur Suryo No. 5, Surabaya" },
-       { lat: -7.966620, lng: 112.632632, title: "Malang", address: "Jl. Ijen No. 6, Malang" },
-       { lat: -0.472448, lng: 117.144128, title: "Balikpapan", address: "Jl. Soekarno Hatta No. 7, Balikpapan" },
-       { lat: -0.322533, lng: 117.578508, title: "Banjarmasin", address: "Jl. A. Yani No. 9, Banjarmasin" },
-       { lat: 1.145207, lng: 109.334868, title: "Pontianak", address: "Jl. Alian No. 5, Pontianak" },
-       { lat: 3.595194, lng: 98.672221, title: "Medan", address: "Jl. Merdeka No. 10, Medan" },
-       { lat: 5.466732, lng: 100.360168, title: "Padang", address: "Jl. Ahmad Yani No. 11, Padang" },
-       { lat: 1.671087, lng: 104.202817, title: "Palembang", address: "Jl. Raya Palembang No. 15, Palembang" },
-       { lat: -0.949954, lng: 119.878724, title: "Makassar", address: "Jl. Losari No. 13, Makassar" },
-       { lat: 0.594269, lng: 122.626142, title: "Manado", address: "Jl. Sam Ratulangi No. 8, Manado" },
-       { lat: -0.548272, lng: 122.982297, title: "Gorontalo", address: "Jl. Sultan Botutihe No. 3, Gorontalo" }
-   ];
+                  const size = Math.min(40 + (markerCount * 2), 80);  // Maksimal ukuran 80px
+                  const radius = size / 2;
 
-   const customIcon = L.icon({
-       iconUrl: 'https://ts3.co.id/assets/upload/image/2.png',
-       iconSize: [32, 32],
-       iconAnchor: [16, 32],
-       popupAnchor: [0, -32]
-   });
+                  // Fungsi untuk menghasilkan warna acak
+                  function getRandomColor() {
+                        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+                  }
 
+                  const randomColor = getRandomColor(); // Dapatkan warna acak
 
-   function initMap() {
-
-       map = L.map('map').setView([ -2.600029, 118.015776 ], 5); // Pusat Indonesia
-
-       // Menambahkan layer OpenStreetMap
-       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-         //   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-         'Streets': L.tileLayer('https://{s}.tile.streets.org/{z}/{x}/{y}.png')
-       }).addTo(map);
-
-       // Tambahkan Semua Marker
-       locations.forEach(location => {
-           const marker = L.marker([location.lat, location.lng], { icon: customIcon }).addTo(map);
-           marker.bindPopup(`
-               <div style="display: flex; align-items: center;">
-                   <img src="https://ts3.co.id/assets/upload/image/2.png" alt="Location Icon" style="width: 50px; height: 50px; margin-right: 10px;">
-                   <div>
-                       <h5>${location.title}</h5>
-                       <p>${location.address}</p>
-                   </div>
-               </div>
-           `);
-
-           marker.on('click', function() {
-               map.setZoom(8);  
-               map.setView([location.lat, location.lng], 8); 
-               marker.openPopup();  
-           });
-
-
-           markers.push(marker);
-       });
-
-
-   }
-
-
-   function changeLocation(value) {
-       if (value === "") {
-           // Jika value kosong, reset peta ke pusat dan tampilkan semua marker
-           map.setView([ -2.600029, 118.015776 ], 5); // Kembali ke posisi pusat Indonesia
-           markers.forEach(marker => marker.addTo(map)); // Menambahkan semua marker ke peta
-       } else {
-           const selectedLocation = locations.find(location => `${location.lat},${location.lng}` === value);
-
-           if (selectedLocation) {
-               const newLocation = { lat: selectedLocation.lat, lng: selectedLocation.lng };
-               map.setView(newLocation, 12);
-               
-             
-               const tempPopup = L.popup()
-                   .setLatLng(newLocation)
-                   .setContent(`
-                       <div style="display: flex; align-items: center;">
-                           <img src="https://ts3.co.id/assets/upload/image/2.png" alt="Location Icon" style="width: 30px; height: 30px; margin-right: 10px;">
-                           <div><h5>${selectedLocation.title}</h5><p>${selectedLocation.address}</p></div>
-                       </div>
-                   `)
-                   .openOn(map);
-           }
-       }
-   }
-
-   // Event listener untuk dropdown
-   document.getElementById('locationSelect').addEventListener('change', function() {
-       const selectedValue = this.value;
-       changeLocation(selectedValue); 
-   });
+                  // Membuat ikon untuk cluster
+                  return L.divIcon({
+                        html: `<div style="background-color: ${randomColor}; border-radius: 50%; width: ${size}px; height: ${size}px; line-height: ${size}px; text-align: center; color: white; font-size: 14px;">${markerCount}</div>`,
+                        className: 'leaflet-markercluster-custom',
+                        iconSize: [size, size],
+                        iconAnchor: [radius, radius],
+                        popupAnchor: [0, -radius]
+                  });
+               }
+            });
 
 
 
 
-   window.onload = initMap;
-</script>
+
+      
+             locations2.forEach(location => {
+                 const marker = L.marker([location.lat, location.lng], { icon: customIcon }).addTo(map);
+                 marker.bindPopup(`
+                     <div style="display: flex; align-items: center;">
+                         <img src="https://ts3.co.id/assets/upload/image/2.png" alt="Location Icon" style="width: 50px; height: 50px; margin-right: 10px;">
+                         <div>
+                             <h5>${location.title}</h5>
+                             <p>${location.address}</p>
+                         </div>
+                     </div>
+                 `);
+      
+                 marker.on('click', function() {
+                     map.setView([location.lat, location.lng], 8); 
+                     marker.openPopup();  
+                 });
+      
+                 markers.push(marker);
+
+                 markersCluster.addLayer(marker);
+             });
+
+             map.addLayer(markersCluster);
+
+               if (map.getZoom() <= 8) {
+                     markers.forEach(marker => marker.setOpacity(0));
+                  }
+
+                 map.on('zoomend', function() {
+                  const zoomLevel = map.getZoom();
+                  if (zoomLevel <= 8) {
+                        // Jika zoom level <= 5, sembunyikan marker
+                        markers.forEach(marker => marker.setOpacity(0));
+                  } else {
+                        // Jika zoom level > 5, tampilkan marker
+                        markers.forEach(marker => marker.setOpacity(1));
+                  }
+               });
+         }
+      
+         function changeLocation(value) {
+             if (value === "") {
+                 map.setView([-2.600029, 118.015776], 5); 
+                 markers.forEach(marker => marker.addTo(map));
+             } else {
+                 const selectedLocation = locations2.find(location => 
+                     `${location.lat},${location.lng}` === value
+                 );
+      
+                 if (selectedLocation) {
+                     const newLocation = { lat: selectedLocation.lat, lng: selectedLocation.lng };
+                     map.setView(newLocation, 12);
+      
+                     const tempPopup = L.popup()
+                         .setLatLng(newLocation)
+                         .setContent(`
+                             <div style="display: flex; align-items: center;">
+                                 <img src="https://ts3.co.id/assets/upload/image/2.png" alt="Location Icon" style="width: 30px; height: 30px; margin-right: 10px;">
+                                 <div><h5>${selectedLocation.title}</h5><p>${selectedLocation.address}</p></div>
+                             </div>
+                         `)
+                         .openOn(map);
+                 }
+             }
+         }
+      
+         document.getElementById('locationSelect').addEventListener('change', function() {
+             const selectedValue = this.value;
+             changeLocation(selectedValue); 
+         });
+      
+         window.onload = initMap;
+      </script>
+      
